996413fc1db5d2631d969a89d9fbd068dcc611d6
smjp7711 <78757005+smjp7711@users.noreply.github.com>
2021-12-13 07:11:31 -0600

    adding dash and home
    adding dash and home

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 const express = require('express');
-const path = require('path');
-
+//const path = require('path');
 const app = express();
+const { users } = require('./data')
+//const projectRouter = require('./routes/projects')
 
-app.get('/', (req, res)=> {
+app.use(express.json()) //pull data from post request/api
+app.use(setUser)
+//app.use('/projects', projectRouter)
+
+app.get('/', (req, res) => {
+    res.send('Home Page')
+  })
+  
+  app.get('/dshbrd', (req, res) => {
+    res.send('Dashboard Page')
+  })
+  
+  app.get('/admin', (req, res) => {
+    res.send('Admin Page')
+  })
+  
+  //Set user based on user ID
+  function setUser(req, res, next) {
+    const userId = req.body.userId
+    if (userId) {
+      req.user = users.find(user => user.id === userId)
+    }
+    next()
+  }
+
+/*app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, 'views/index.html'));
-});
+});*/
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, _=>{
-    console.log('This is my new page');
+    console.log('Server is running, YAY');
 });
 
